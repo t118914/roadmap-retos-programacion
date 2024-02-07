e0313282f9b1769f15f7e690d9d044d3a7d0189d
t118914 <129491490+t118914@users.noreply.github.com>
2024-02-07 12:34:17 +0100

    Create Reto1.java

---
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/java/Reto1.java
@@ -0,0 +1,14 @@
+package main;
+
+public class Main {
+	public static void main(String args[]) {
+		for(int i = 10; i < 56; i++) {
+			if(i == 16 || (i % 3) == 0) {
+				
+			}
+			else {
+				System.out.println(i);
+			}
+		}
+	}
+}
